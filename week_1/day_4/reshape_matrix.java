bb8d8339761e1a3570d0713848cd75b7f1c0439e
Adarsh_0018 <singhadarshsingh461@gmail.com>
2026-06-04 22:46:36 +0530

    Week 1 Day 4 solution

---
+++ week_1/day_4/reshape_matrix.java
@@ -0,0 +1,14 @@
+int m = mat.length;
+int n = mat[0].length;
+
+        if (m * n != r * c) {
+        return mat;
+        }
+
+int[][] res = new int[r][c];
+
+        for (int i = 0; i < m * n; i++) {
+res[i / c][i % c] = mat[i / n][i % n];
+        }
+
+        return res;
